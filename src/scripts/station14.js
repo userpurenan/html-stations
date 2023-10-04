89f25370e3b326181b8bcee87e770fb1f991f796
userpurenan <nan691628@gmail.com>
2023-10-04 16:10:57 +0900

    clear

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -6,6 +6,7 @@ function getData() {
         { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
         { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
     ];
+
     return test.map(buildFullName)
 }
 
@@ -13,6 +14,11 @@ function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+    const full_name = `${data.family_name} ${data.first_name}`;
+    return {
+        ...data,
+        full_name
+    };
 }
 
 // 出力が想定される答え
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,21 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const updatedUserList = userList.map(buildFullName);
+      resolve(updatedUserList);
+    }, 3000); // 3秒後に解決（resolve）されるPromiseを返す
+  });
 }
 
+function buildFullName(data){
+
+       data["full_name"] = data.family_name + " " + data.first_name
+    return data
+}
